6f427782a4397f78bdb33f32983c89a3e49d53cd
MSFauzan505 <mshafauzan01@gmail.com>
2024-09-30 13:09:21 +0700

    nambah pages dan routes

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Project from './pages/Project'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/project' element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
