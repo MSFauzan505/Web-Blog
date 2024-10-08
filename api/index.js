6555961612f79808a0305bbddd32f06b3ec00697
MSFauzan505 <mshafauzan01@gmail.com>
2024-10-08 16:25:30 +0700

    buat signup API router

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ mongoose
 
 const app = express();
 
-
+app.use(express.json());
 app.listen(3000, () => {
 
     console.log('Server is running on port 3000!');
@@ -24,3 +25,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
